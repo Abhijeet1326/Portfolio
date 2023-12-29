ccdae0e7adb33159dfa750ff822c26ac9a50a35d
Abhijeet1326 <buntysadiq@gmail.com>
2023-12-29 13:59:35 +0530

    Untitled image correction

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Layout from '@/components/Layout'
 import Head from 'next/head'
 import React, { useEffect, useRef } from 'react'
 import Image from 'next/image'
-import profilePic from "../../public/images/profile/untitled.png"
+import profilePic from "../../public/images/profile/Untitled.png"
 import { useInView, useMotionValue, useSpring } from 'framer-motion'
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
